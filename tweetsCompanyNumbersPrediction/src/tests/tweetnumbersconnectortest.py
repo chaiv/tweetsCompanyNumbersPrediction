01c4d1c6806cda4109994f2d7667036518041427
vital <vital@LAPTOP-BANG055V.fritz.box>
2022-01-22 16:19:22 +0100

    master creation commit

---
+++ tweetsCompanyNumbersPrediction/src/tests/tweetnumbersconnectortest.py
@@ -0,0 +1,21 @@
+'''
+Created on 22.01.2022
+
+@author: vital
+'''
+import unittest
+from tweetnumbersconnector.tweetnumbersconnector import TweetNumbersConnector
+
+
+
+class Test(unittest.TestCase):
+
+
+    def testTweetNumbersConnector(self):
+        connector = TweetNumbersConnector()
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ tweetsCompanyNumbersPrediction/src/tweetnumbersconnector/tweetnumbersconnector.py
@@ -0,0 +1,18 @@
+'''
+Created on 22.01.2022
+
+@author: vital
+'''
+
+class TweetNumbersConnector(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+    def getTweets(self,allTweets, allNumbers):    
+        return []
