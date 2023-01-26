6408934b32abd5670fc9bc773d40c73a31b594f4
vital <vital@LAPTOP-BANG055V.fritz.box>
2023-01-26 19:40:29 +0100

    trainer refactoring

--- tweetsCompanyNumbersPrediction/src/classifier/Dataloader.py
+++ tweetsCompanyNumbersPrediction/src/classifier/Dataloader.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 
 class Dataset(Dataset):
-    def __init__(self,x,y,deviceToUse=torch.device("cuda:0")):
+    def __init__(self,x,y,deviceToUse):
         self.x = torch.tensor(x,dtype=torch.float32).to(deviceToUse )
         self.y = torch.tensor(y,dtype=torch.float32).to(deviceToUse )
         self.length = self.x.shape[0]
@@ -19,15 +19,16 @@ class Dataset(Dataset):
 
 class Dataloader(object):
 
-    def __init__(self,xTrain,yTrain,xTest=None,yTest=None, batch_size=258944 ,shuffle=False):
+    def __init__(self,xTrain,yTrain,xTest=None,yTest=None, batch_size=258944 ,shuffle=False,deviceToUse=torch.device("cuda:0")):
         self.xTrain = xTrain
         self.yTrain = yTrain
         self.xTest = xTest
         self.yTest = yTest
         self.batch_size = batch_size
         self.shuffle = shuffle
+        self.deviceToUse = deviceToUse
     
     def getTrainsetDataloader(self):
-        return DataLoader(Dataset(self.xTrain,self.yTrain),self.batch_size ,self.shuffle)
+        return DataLoader(Dataset(self.xTrain,self.yTrain,self.deviceToUse),self.batch_size ,self.shuffle)
     
         
--- tweetsCompanyNumbersPrediction/src/classifier/FNNTest.py
+++ tweetsCompanyNumbersPrediction/src/classifier/FNNTest.py
@@ -4,6 +4,7 @@ from tweetpreprocess.DataDirHelper import DataDirHelper
 from classifier.Dataloader import Dataloader
 from classifier.FNN import FNN
 from torch import nn
+from classifier.Trainer import Trainer
 
 featuresDf = pd.read_pickle(DataDirHelper().getDataDir()+ 'companyTweets\\featuresClassesAmazon.pkl')
 #featuresDf = pd.read_csv(DataDirHelper().getDataDir()+ 'companyTweets\\FeaturesClassesAAPLFirst1000.csv')
@@ -15,34 +16,9 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 x = sc.fit_transform(x)
 
-deviceToUse = torch.device("cuda:0")
-trainloader =  Dataloader(x,y).getTrainsetDataloader()   
+deviceToUse = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+trainloader =  Dataloader(x,y,deviceToUse=deviceToUse ).getTrainsetDataloader()   
     
-#hyper parameters
-learning_rate = 0.01
-epochs = 700# Model , Optimizer, Loss
-model = FNN(input_shape=x.shape[1]).to(deviceToUse)
-optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate)
-loss_fn = nn.BCELoss()
-
-#forward loop
-losses = []
-accur = []
-for i in range(epochs):
-    for j,(x_train,y_train) in enumerate(trainloader):
-    
-    #calculate output
-        output = model(x_train.to(deviceToUse ))
- 
-    #calculate loss
-        loss = loss_fn(output,y_train.reshape(-1,1))
- 
-    #accuracy
-        predicted = model(torch.tensor(x,dtype=torch.float32).to(deviceToUse ))
-        acc = (predicted.reshape(-1).detach().cpu().numpy().round() == y).mean()    #backprop
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        losses.append(loss)
-        accur.append(acc)
-        print("epoch {}\tloss : {}\t accuracy : {}".format(i,loss,acc))
+model = FNN(input_shape=x.shape[1],deviceToUse=deviceToUse).to(deviceToUse)
+trainer = Trainer(loss_function = nn.BCELoss(), optimizer = torch.optim.SGD(model.parameters(),lr=0.01),epochs = 10, deviceToUse=deviceToUse)
+trainer.train(model, x, y, trainloader)
