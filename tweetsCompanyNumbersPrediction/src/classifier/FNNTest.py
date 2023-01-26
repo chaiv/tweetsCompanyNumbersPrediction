4afa197b3340366de561b9d56d3f33b57d6e8344
vital <vital@LAPTOP-BANG055V.fritz.box>
2023-01-26 22:39:23 +0100

    trainer with validate set

--- tweetsCompanyNumbersPrediction/src/classifier/FNNTest.py
+++ tweetsCompanyNumbersPrediction/src/classifier/FNNTest.py
@@ -6,19 +6,34 @@ from classifier.FNN import FNN
 from torch import nn
 from classifier.Trainer import Trainer
 
+#https://stats.stackexchange.com/questions/258166/good-accuracy-despite-high-loss-value
+
 featuresDf = pd.read_pickle(DataDirHelper().getDataDir()+ 'companyTweets\\featuresClassesAmazon.pkl')
-#featuresDf = pd.read_csv(DataDirHelper().getDataDir()+ 'companyTweets\\FeaturesClassesAAPLFirst1000.csv')
-x = featuresDf.iloc[:,1:301].to_numpy()
+#featuresDf = pd.read_csv(DataDirHelper().getDataDir() + 'companyTweets\\FeaturesClassesAAPLFirst1000.csv')
+train_endrow = int((len(featuresDf.index) * 0.7))
+x = featuresDf.iloc[:, 1:301].to_numpy()
 y = featuresDf["class"].to_numpy()
 
-#feature scaling
+# feature scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 x = sc.fit_transform(x)
 
+x_train = x[:train_endrow]
+y_train = y[:train_endrow]
+
+x_test = x[train_endrow:]
+y_test = y[train_endrow:]
+
 deviceToUse = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-trainloader =  Dataloader(x,y,deviceToUse=deviceToUse ).getTrainsetDataloader()   
+trainloader = Dataloader(x, y, deviceToUse=deviceToUse).getTrainsetDataloader()   
     
-model = FNN(input_shape=x.shape[1],deviceToUse=deviceToUse).to(deviceToUse)
-trainer = Trainer(loss_function = nn.BCELoss(), optimizer = torch.optim.SGD(model.parameters(),lr=0.01),epochs = 10, deviceToUse=deviceToUse)
-trainer.train(model, x, y, trainloader)
+model = FNN(input_shape=x.shape[1], deviceToUse=deviceToUse).to(deviceToUse)
+trainer = Trainer(
+    loss_function=nn.BCELoss(),
+    optimizer=torch.optim.SGD(model.parameters(), lr=0.01), 
+    model=model, 
+    epochs=50, 
+    deviceToUse=deviceToUse
+    )
+trainer.train(x_test, y_test, trainloader)
--- tweetsCompanyNumbersPrediction/src/classifier/Trainer.py
+++ tweetsCompanyNumbersPrediction/src/classifier/Trainer.py
@@ -6,27 +6,40 @@ Created on 25.01.2023
 import torch
 class Trainer(object):
 
-    def __init__(self,loss_function,optimizer, epochs = 10,deviceToUse = torch.device("cuda:0") ):
+    def __init__(self,loss_function,optimizer, model, epochs = 10,deviceToUse = torch.device("cuda:0") ):
         self.epochs = epochs
         self.deviceToUse = deviceToUse
         self.loss_function = loss_function
         self.optimizer = optimizer
+        self.model = model
         self.losses = []
         self.accur = []
+        
+        
+    def test(self):
+        pass
     
-    def train(self,model,x_data,y_data,trainloader):
+    
+    def calculateAccuracy(self,x,y):
+        predicted = self.model(torch.tensor(x,dtype=torch.float32).to(self.deviceToUse ))
+        return (predicted.reshape(-1).detach().cpu().numpy().round() == y).mean() 
+    
+    def calculateLoss(self, y, predicted):
+        return self.loss_function(predicted, y.reshape(-1, 1))
+    
+    def train(self,x_test,y_test,trainloader):
         for i in range(self.epochs):
             for j,(x_train,y_train) in enumerate(trainloader):
     
                 #calculate output
-                output = model(x_train)
+                output = self.model(x_train)
  
                 #calculate loss
-                loss =  self.loss_function(output,y_train.reshape(-1,1))
+                loss = self.calculateLoss(y_train.reshape(-1,1),output)
  
                 #accuracy
-                predicted = model(torch.tensor(x_data,dtype=torch.float32).to(self.deviceToUse ))
-                acc = (predicted.reshape(-1).detach().cpu().numpy().round() == y_data).mean()    #backprop
+                acc = self.calculateAccuracy(x_test,y_test)
+                #backprop
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
