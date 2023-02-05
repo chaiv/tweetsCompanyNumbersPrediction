1ea4febe34fd7786a97537b301756ffee8f5d16d
vital <vital@DESKTOP-BE0958L.fritz.box>
2023-02-05 18:53:07 +0100

    model predict

--- tweetsCompanyNumbersPrediction/src/extractMostImportantWords.py
+++ tweetsCompanyNumbersPrediction/src/extractMostImportantWords.py
@@ -11,9 +11,15 @@ from tweetpreprocess.DataDirHelper import DataDirHelper
 tokenizer = TokenizerTop2Vec(DataDirHelper().getDataDir()+ "companyTweets\TokenizerAmazon.json")
 vocab_size = tokenizer.getVocabularyLength()
 model = Transformer(lr=1e-4, n_outputs=2, vocab_size=vocab_size+2)
-model.load_state_dict(torch.load(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonTweetpredict.ckpt"),strict=False)
+checkpoint = torch.load(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonTweetpredict.ckpt")
+model.load_state_dict(checkpoint['state_dict'])
 model.eval()
-x_Tokenids = tokenizer.encode("S&P100 #Stocks Performance $HD $LOW $SBUX $TGT $DVN $IBM $AMZN $F $APA $GM $MS $HAL $DIS $MCD $BMY $XOM  more@")
-model(
-    torch.tensor(x_Tokenids , dtype=torch.long)
-    )
+#text = "$AMZN - 21st Century Fox Earnings: An EPS Beat, but Revenue Misses Expectations" 
+text = "S&P100 #Stocks Performance $HD $LOW $SBUX $TGT $DVN $IBM $AMZN $F $APA $GM $MS $HAL $DIS $MCD $BMY $XOM  more@"
+x_Tokenids = tokenizer.encode(text)
+x_Tokenids  += [tokenizer.getPADTokenID()]*(256 - len(x_Tokenids)) #padding
+with torch.no_grad():
+    pred = model(torch.tensor(x_Tokenids , dtype=torch.long))
+    pred = torch.sigmoid(pred)
+    pred = torch.round(pred)
+    print(pred)
