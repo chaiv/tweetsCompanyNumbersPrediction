84d80e0ae941a95e15f005aed3df5ca18741dedd
vital <vital@DESKTOP-BE0958L.fritz.box>
2023-03-07 13:01:37 +0100

    manual topic lookup in misc

--- tweetsCompanyNumbersPrediction/src/misc.py
+++ tweetsCompanyNumbersPrediction/src/misc.py
@@ -7,5 +7,5 @@ from nlpvectors.TweetTokenizer import TweetTokenizer
 from tweetpreprocess.wordfiltering.DefaultWordFilter import DefaultWordFilter
 modelpath =  DataDirHelper().getDataDir()+ "companyTweets\\amazonTopicModelV2"
 topicExtractor = TopicExtractor(TopicModelCreator().load(modelpath))
-topic_words, word_scores, topic_nums = topicExtractor.searchTopics(TweetTokenizer(DefaultWordFilter()).tokenize("refugee"), 2)
+topic_words,word_scores,topic_scores,topic_nums = topicExtractor.searchTopics(TweetTokenizer(DefaultWordFilter()).tokenize('Greek debt crisis'), 2)
 print(topic_words)
