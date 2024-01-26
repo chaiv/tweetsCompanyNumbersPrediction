3afc60e0c2ee48299d7af935d374d7b64295aa1f
vital <vital@DESKTOP-BE0958L.fritz.box>
2024-01-26 18:37:16 +0100

    all tweet groups of test fold to df

--- tweetsCompanyNumbersPrediction/src/createTweetGroupDataframe.py
+++ tweetsCompanyNumbersPrediction/src/createTweetGroupDataframe.py
@@ -6,17 +6,30 @@ Created on 15.01.2024
 from nlpvectors.TweetGroupToDataframe import TweetGroupToDataframe
 from tweetpreprocess.DataDirHelper import DataDirHelper
 import pandas as pd
+import numpy as np
 from nlpvectors.DataframeSplitter import DataframeSplitter
 import random
+from classifier.ModelEvaluationHelper import createTweetGroupsAndTrueClasses
+from gensim.models import KeyedVectors
+from nlpvectors.WordVectorsIDEncoder import WordVectorsIDEncoder
+from tweetpreprocess.wordfiltering.DefaultWordFilter import DefaultWordFilter
+from nlpvectors.TweetTokenizer import TweetTokenizer
 
 tweetDf = pd.read_csv(DataDirHelper().getDataDir()+"companyTweets\\amazonTweetsWithNumbers.csv")
 tweetDf.fillna('', inplace=True) #nan values in body columns 
-splitter = DataframeSplitter()
-splits = splitter.getSplitIds(tweetDf, 5) #how many tweets  as one sample
-random_splits = random.sample(splits, 100)
-for split in random_splits:
-    splitDf =  tweetDf [ tweetDf ["tweet_id"].isin( split)]
-    sentences = tweetDf ["body"].tolist()
-    label = tweetDf["class"].iloc[0]
-
-#tweetGroupDf.to_csv(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonRevenueLSTMN5\\tweetGroups5.csv")
+word_vectors = KeyedVectors.load_word2vec_format(DataDirHelper().getDataDir()+ "companyTweets\\WordVectorsAmazonV2.txt", binary=False)
+textEncoder = WordVectorsIDEncoder(word_vectors)
+tokenizer = TweetTokenizer(DefaultWordFilter())
+df = pd.read_csv(DataDirHelper().getDataDir()+ 'companyTweets\\amazonTweetsWithNumbers.csv')
+df.fillna('', inplace=True)
+testSplitIndexes = np.load(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonRevenueLSTMN5\\test_idx_fold1.npy")
+tweetGroups,trueClasses = createTweetGroupsAndTrueClasses(
+        df,
+        5,
+        testSplitIndexes,
+        tokenizer,
+        textEncoder
+        )
+tweetGroupToDataframe = TweetGroupToDataframe()
+tweetGroupDf = tweetGroupToDataframe.createTweetGroupDataframe(tweetGroups)
+tweetGroupDf.to_csv(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonRevenueLSTMN5\\tweetGroups_at_5.csv")
