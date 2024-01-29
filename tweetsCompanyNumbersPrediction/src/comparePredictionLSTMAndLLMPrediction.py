473086e98b7615ae1b0c9c914f7671d9e729017d
vital <vital@DESKTOP-BE0958L.fritz.box>
2024-01-29 18:58:41 +0100

    compare prediction llm metrics

--- tweetsCompanyNumbersPrediction/src/comparePredictionLSTMAndLLMPrediction.py
+++ tweetsCompanyNumbersPrediction/src/comparePredictionLSTMAndLLMPrediction.py
@@ -3,3 +3,12 @@ Created on 27.01.2024
 
 @author: vital
 '''
+import pandas as pd
+from tweetpreprocess.DataDirHelper import DataDirHelper
+from classifier.BinaryClassificationMetrics import BinaryClassificationMetrics
+
+tweetGroupDf = pd.read_csv(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonRevenueLSTMN5\\tweetGroups_at_5_first_N.csv")
+true_classes = tweetGroupDf["class"].tolist()
+llm_prediction_classes = tweetGroupDf["chatgpt_class"].tolist()
+metrics = BinaryClassificationMetrics() 
+print(metrics.classification_report(true_classes, llm_prediction_classes ))
