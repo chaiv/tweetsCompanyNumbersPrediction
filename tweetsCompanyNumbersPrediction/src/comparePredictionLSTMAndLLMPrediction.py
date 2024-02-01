63a807061df2137829c52b116418ef8be31d21be
vital <vital@DESKTOP-BE0958L.fritz.box>
2024-02-01 20:48:59 +0100

    first llm prediction classes sample

--- tweetsCompanyNumbersPrediction/src/comparePredictionLSTMAndLLMPrediction.py
+++ tweetsCompanyNumbersPrediction/src/comparePredictionLSTMAndLLMPrediction.py
@@ -9,6 +9,44 @@ from classifier.BinaryClassificationMetrics import BinaryClassificationMetrics
 
 tweetGroupDf = pd.read_csv(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonRevenueLSTMN5\\tweetGroups_at_5_first_N.csv")
 true_classes = tweetGroupDf["class"].tolist()
-llm_prediction_classes = tweetGroupDf["chatgpt_class"].tolist()
+llm_prediction_classes = [
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+
+1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+
+0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0,
+
+0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0,
+
+1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+
+1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0,
+
+0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0,
+
+0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0,
+
+1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0,
+
+0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 
+
+1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 
+
+0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+
+0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+
+0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+
+0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0,
+
+1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+
+1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0,
+
+0.0
+]
+#tweetGroupDf["chatgpt_class"].tolist()
 metrics = BinaryClassificationMetrics() 
+print(len(true_classes),len(llm_prediction_classes))
 print(metrics.classification_report(true_classes, llm_prediction_classes ))
--- tweetsCompanyNumbersPrediction/src/selectTweetGroupsFromTweetGroupDataframe.py
+++ tweetsCompanyNumbersPrediction/src/selectTweetGroupsFromTweetGroupDataframe.py
@@ -7,8 +7,9 @@ import pandas as pd
 from tweetpreprocess.DataDirHelper import DataDirHelper
 
 tweetDf = pd.read_csv(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonRevenueLSTMN5\\tweetGroups_at_5.csv")
-firstNZeroClasses = tweetDf[tweetDf["class"]==0.0].head(25)
-firstNOneClasses = tweetDf[tweetDf["class"]==1.0].head(25)
+firstNZeroClasses = tweetDf[tweetDf["class"]==0.0].head(100)
+firstNOneClasses = tweetDf[tweetDf["class"]==1.0].head(100)
 firstNTweetGroupsDfRandomOrder = pd.concat([firstNZeroClasses, firstNOneClasses], ignore_index=True).sample(frac=1)
+firstNTweetGroupsDfRandomOrder["tweet_sentences"] = firstNTweetGroupsDfRandomOrder["tweet_sentences"].apply(lambda x: str(x) + "<SEP>")
 firstNTweetGroupsDfRandomOrder.to_csv(DataDirHelper().getDataDir()+"companyTweets\\model\\amazonRevenueLSTMN5\\tweetGroups_at_5_first_N.csv")
 
