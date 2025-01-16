0d8cf5de4a1a34af4fc2e35a448c22232f45489b
vital <vital@DESKTOP-BE0958L.fritz.box>
2025-01-16 20:16:23 +0100

    further get scores and topics for manual topic script

--- tweetsCompanyNumbersPrediction/src/getScoresAndTopicsForAManualTopic.py
+++ tweetsCompanyNumbersPrediction/src/getScoresAndTopicsForAManualTopic.py
@@ -19,8 +19,29 @@ from collections import Counter
 from classifier.ClassificationMetrics import ClassificationMetrics
 from featureinterpretation.ImportantWordsStore import createImportantWordStore
 from featureinterpretation.AttributionsCalculator import AttributionsCalculator
+from nlpvectors.TokenizerWordsLookup import TokenizedWordsLookup
 
-manualTopic = "Climate Change"
+def transform_token(tokenizerLookUp,token):
+    if tokenizerLookUp.hasOriginalWord(token):
+        return tokenizerLookUp.getOriginalWord(token)
+    return token
+
+
+def getMostImportantWordsForClass(predictor,tweetGroups, preditionClass,topNImportantWords):
+    wordScoresWrappers = predictor.calculateWordScoresOfTweetGroupsInChunks(tweetGroups,observed_class=preditionClass,chunkSize=100,n_steps=500,internal_batch_size = 100)
+    importantWordsStore = createImportantWordStore(wordScoresWrappers,prediction_classes)
+    importantWordsDf = importantWordsStore.to_dataframe()
+    importantWordsDf= importantWordsDf.sort_values(by=["tweet_attribution", "token_attribution"], ascending=[False, False])
+    importantWordsDf= importantWordsDf.drop_duplicates(subset=["token"], keep="first")
+    topNImportantWords = list(
+        zip(
+            [transform_token(tokenizerLookUp,token) for token in importantWordsDf["token"].head(topNImportantWords)],
+            importantWordsDf["token_attribution"].head(topNImportantWords)
+        )
+    )
+    return topNImportantWords
+
+manualTopic = "Black Lives Matter Movement "
 topNImportantWords = 10
 predictionModelPath = AMAZON_REVENUE_10_LSTM_BINARY_CLASS
 predictionClassMapper = predictionModelPath.getPredictionClassMapper()
@@ -37,11 +58,14 @@ fold = 0
 modelPath = predictionModelPath.getModelPath()+"\\tweetpredict_fold"+str(fold)+".ckpt"
 model = loadModel(modelPath,word_vectors,num_classes=predictionClassMapper.get_number_of_classes(),evalMode=False)
 predictor = Predictor(model,tokenizer,textEncoder ,predictionClassMapper ,AttributionsCalculator(model,model.embedding))
+tokenizerLookUp = TokenizedWordsLookup(predictionModelPath.getModelPath()+"\\tokensDictionary.json")
+
+
 
 topic_words,word_scores,topic_scores,topic_nums  = topicExtractor.findTopics(manualTopic, 10)
 document_ids = []
 for topic_num in topic_nums:
-    document_ids.extend(topicExtractor.get_document_ids_by_topic(topic_num, num_docs=10))
+    document_ids.extend(topicExtractor.get_document_ids_by_topic(topic_num))
 filtered_df = df[df["tweet_id"].isin(document_ids)]
 tweetSplits = DataframeSplitter().getSplitIds(filtered_df,predictionModelPath.getTweetGroupSize())
 tweetGroups = []
@@ -61,11 +85,8 @@ print("prediction_classes counts ",', '.join(f"{item}: {count}" for item, count 
 metrics = ClassificationMetrics() 
 print(metrics.classification_report(trueClasses, prediction_classes))
 print("MCC "+str(metrics.calculate_mcc(trueClasses, prediction_classes)))
-wordScoresWrappers = predictor.calculateWordScoresOfTweetGroupsInChunks(tweetGroups,observed_class=1,chunkSize=100,n_steps=500,internal_batch_size = 100)
-importantWordsStore = createImportantWordStore(wordScoresWrappers,prediction_classes)
-importantWordsDf = importantWordsStore.to_dataframe()
-importantWordsDf= importantWordsDf.sort_values(by=["tweet_attribution", "token_attribution"], ascending=[False, False])
-importantWordsDf= importantWordsDf.drop_duplicates(subset=["token"], keep="first")
-topNImportantWords = list(zip(importantWordsDf["token"].head(topNImportantWords), importantWordsDf["token_attribution"].head(topNImportantWords)))
-print(topNImportantWords)
+print(getMostImportantWordsForClass(predictor,tweetGroups, 0,topNImportantWords))
+print(getMostImportantWordsForClass(predictor,tweetGroups, 1,topNImportantWords))
+
+
 
